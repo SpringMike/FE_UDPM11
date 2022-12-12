5841852b803df81a49e39f99bb0bac7e17b0f559
ducnv2509 <ducit2509@gmail.com>
2022-12-12 21:38:24 +0700

    fix API

--- src/admin/service/ManagerOrderAdmin.ts
+++ src/admin/service/ManagerOrderAdmin.ts
@@ -9,41 +9,41 @@ const headers = {
 
 
 export const getAllOrder = async () => {
-    return await axios.get(`${base_url}/manager-oder/findAll`, { headers })
+    return await axios.get(`${base_url}/api/manager-oder/findAll`, { headers })
 }
 
 export const updateStatusOrderByAdmin = async (status_id: number, list_id_order: number[], action_by: String) => {
-    return await axios.put(`${base_url}/manager-oder/update-multiple/${status_id}?action_by=${action_by}`, list_id_order, { headers })
+    return await axios.put(`${base_url}/api/manager-oder/update-multiple/${status_id}?action_by=${action_by}`, list_id_order, { headers })
 
 }
 export const getOrderItemsByIdOrder = async (id_order: number) => {
-    return await axios.get(`${base_url}/manager-oder/showItem/${id_order}`, { headers })
+    return await axios.get(`${base_url}/api/manager-oder/showItem/${id_order}`, { headers })
 }
 
 
 export const findAllOrderByStatus = async (status_id: number) => {
-    return await axios.put(`${base_url}/manager-oder/findAll/by/${status_id}`, { headers })
+    return await axios.put(`${base_url}/api/manager-oder/findAll/by/${status_id}`, { headers })
 }
 
 
 // order return
 export const getAllOrderReturn = async () => {
-    return await axios.get(`${base_url}/manager-oder/findAll/orderReturn`, { headers })
+    return await axios.get(`${base_url}/api/manager-oder/findAll/orderReturn`, { headers })
 }
 
 export const updateStatusReturnOrderByAdmin = async (status_id: number, id_order: number) => {
-    return await axios.put(`${base_url}/manager-oder/update-return-status/${status_id}?idOrderReturn=${id_order}`, { headers })
+    return await axios.put(`${base_url}/api/manager-oder/update-return-status/${status_id}?idOrderReturn=${id_order}`, { headers })
 
 }
 export const getOrderReturnItemsByIdOrder = async (id_order_return: number) => {
-    return await axios.get(`${base_url}/manager-oder/showItem/orderReturn/${id_order_return}`, { headers })
+    return await axios.get(`${base_url}/api/manager-oder/showItem/orderReturn/${id_order_return}`, { headers })
 }
 
 // search
 
 export const searchOrderAll = async (query: String) => {
-    return await axios.get(`${base_url}/manager-oder/searchAllOrder?query=${query}`, { headers })
+    return await axios.get(`${base_url}/api/manager-oder/searchAllOrder?query=${query}`, { headers })
 }
 export const searchOrderReturnAll = async (query: String) => {
-    return await axios.get(`${base_url}/manager-oder/searchOrdersReturn?query=${query}`, { headers })
+    return await axios.get(`${base_url}/api/manager-oder/searchOrdersReturn?query=${query}`, { headers })
 }
